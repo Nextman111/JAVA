c80db14b4c8227c6d7aa9e37a03e4f4fb30db266
VladislavRazvodov <g-man-111@ya.ru>
2023-02-09 08:30:21 +0300

    homework seminar3

--- Seminar/src/main/java/seminar3/task3.java
+++ Seminar/src/main/java/seminar3/task3.java
@@ -1,4 +1,71 @@
 package seminar3;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class task3 {
+    public static void main(String[] args) {
+//        task1();
+//        task2();
+        task3();
+    }
+    private static void task1() {
+        /**1. Пусть дан произвольный список целых чисел, удалить из него четные числа (в языке уже есть что-то готовое для этого)*/
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        int listLength = 10;
+
+        for (int i = 0; i < listLength; i++) {
+            list.add(random.nextInt(11));
+        }
+
+        System.out.println(list);
+
+        list = list.stream().filter(x -> x%2 == 1).collect(Collectors.toList());
+
+        System.out.println(list);
+
+    }
+    private static void task2() {
+        /**2. Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое из этого списка.*/
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        int listLength = 10;
+
+        for (int i = 0; i < listLength; i++) {
+            list.add(random.nextInt(100));
+        }
+
+        System.out.println(list);
+
+        System.out.printf("min = %d%n", Collections.min(list));
+        System.out.printf("max = %d%n", Collections.max(list));
+
+        int average = list.stream().mapToInt(x-> x).sum() / list.size();
+        System.out.printf("average = %d%n", average);
+
+    }
+
+    private static void task3() {
+        /**3. Пройти по списку из задания 2 с урока и удалить повторяющиеся элементы.*/
+        String[] solarSystemPlanets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+
+        Random random = new Random();
+        List<String> planetList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            planetList.add(solarSystemPlanets[random.nextInt(0,solarSystemPlanets.length)]);
+        }
+
+        System.out.println(planetList);
+
+        for (String item: solarSystemPlanets) {
+            System.out.println(item + " : " + Collections.frequency(planetList,item));
+        }
+
+        planetList = planetList.stream().collect(Collectors.toSet()).stream().collect(Collectors.toList());
+
+        System.out.println(planetList);
+    }
 }
