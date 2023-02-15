538ec751cd25a0c48f00710b3c175ae2bdfbcda4
VladislavRazvodov <g-man-111@ya.ru>
2023-02-15 04:15:17 +0300

    add new version for methods homework4

--- Seminar/src/main/java/seminar4/homework4.java
+++ Seminar/src/main/java/seminar4/homework4.java
@@ -4,12 +4,13 @@ import java.util.*;
 
 public class homework4 {
     public static void main(String[] args) {
-        task1();
+//        task1();
+        task1_1();
 //        task2();
     }
 
-        /**2. Пусть дан LinkedList с несколькими элементами.
-         * Реализуйте метод, который вернет “перевернутый” список.*/
+/**2. Пусть дан LinkedList с несколькими элементами.
+ * Реализуйте метод, который вернет “перевернутый” список.*/
     private static void task2() {
         LinkedList<Integer> linkedList = new LinkedList<>();
         Random random = new Random();
@@ -20,7 +21,8 @@ public class homework4 {
         }
 
         System.out.println(linkedList);
-        System.out.println(reverseIntList(linkedList));
+//        System.out.println(reverseIntList(linkedList));
+        System.out.println(reverseIntListV2(linkedList));
         System.out.println(linkedList);
     }
 
@@ -33,6 +35,19 @@ public class homework4 {
         return reverseList;
     }
 
+    public static LinkedList<Integer> reverseIntListV2(LinkedList<Integer> linkedList){
+        if (linkedList.size() < 1){ return linkedList; }
+
+        int temp = linkedList.getFirst();
+
+        for (int i = 0, j = linkedList.size()-1; i < j;  i++, j--) {
+            linkedList.set(i, linkedList.get(j));
+            linkedList.set(j, temp);
+            temp = linkedList.get(i+1);
+        }
+
+        return linkedList;
+    }
 
     public static LinkedList<String> reverseStringList(LinkedList<String> linkedList){
         LinkedList<String> reverseList = new LinkedList<>();
@@ -44,11 +59,11 @@ public class homework4 {
     }
 
 
-        /**1. Реализовать консольное приложение, которое:
-         Принимает от пользователя и “запоминает” строки.
-         Если введено print, выводит строки так,
-         чтобы последняя введенная была первой в списке, а первая - последней.
-         Если введено revert, удаляет предыдущую введенную строку из памяти.*/
+/**1. Реализовать консольное приложение, которое:
+ Принимает от пользователя и “запоминает” строки.
+ Если введено print, выводит строки так,
+ чтобы последняя введенная была первой в списке, а первая - последней.
+ Если введено revert, удаляет предыдущую введенную строку из памяти.*/
     private static void task1() {
         String userString;
         LinkedList<String> linkedList = new LinkedList<>();
@@ -63,13 +78,9 @@ public class homework4 {
                 continue;
             }
             switch (userString.trim()) {
-                case ("help"):
-                    task1Help();
-                    break;
-                case ("print"):
-                    System.out.println(reverseStringList(linkedList));
-                    break;
-                case ("revert"):
+                case ("help") -> task1Help();
+                case ("print") -> System.out.println(reverseStringList(linkedList));
+                case ("revert") -> {
                     if (linkedList.size() > 0) {
                         linkedList.removeLast();
                         System.out.println("Посленее добавление удалено");
@@ -77,15 +88,52 @@ public class homework4 {
                     } else {
                         System.out.println("Список пуст");
                     }
-                    break;
-                case ("exit"):
+                }
+                case ("exit") -> {
                     System.out.println("Программа остановлена");
                     System.exit(0);
-                    break;
-                default:
+                }
+                default -> {
                     linkedList.add(userString);
                     System.out.println("Запись добавлена");
-                    break;
+                }
+            }
+        }
+    }
+
+    private static void task1_1() {
+        String userString;
+        Deque<String> arrayDeque = new ArrayDeque<>();
+
+        task1Help();
+
+        while (true) {
+            System.out.println("Введите строку для добавления в список: ");
+            userString = getStringFromUser();
+            if (userString.isBlank()){
+                System.out.println("Строка пустая");
+                continue;
+            }
+            switch (userString.trim()) {
+                case ("help") -> task1Help();
+                case ("print") -> System.out.println(arrayDeque);
+                case ("revert") -> {
+                    if (arrayDeque.size() > 0) {
+                        arrayDeque.removeFirst();
+                        System.out.println("Посленее добавление удалено");
+                        System.out.println(arrayDeque);
+                    } else {
+                        System.out.println("Список пуст");
+                    }
+                }
+                case ("exit") -> {
+                    System.out.println("Программа остановлена");
+                    System.exit(0);
+                }
+                default -> {
+                    arrayDeque.addFirst(userString);
+                    System.out.println("Запись добавлена");
+                }
             }
         }
     }
